b003c29b01a7213418c8b523f952b734695bd3b4
Jacqueline Delgado <jdboston.bu@gmail.com>
2026-06-01 10:10:44 -0400

    updated a video and added a short movie

--- script.js
+++ script.js
@@ -225,6 +225,21 @@ window.toggleMenu = function() {
 
 
 function openProject(projectId) {
+    const iframe = event.currentTarget.querySelector('iframe');
+    const cover = event.currentTarget.querySelector('.youtube-cover');
+    const overlay = event.currentTarget.querySelector('.work-overlay');
+
+    if (iframe) {
+        iframe.src = "https://www.youtube.com/embed/JOhiWY7XmoY?autoplay=1&mute=0&controls=1&rel=0";
+        if (cover) {
+            cover.style.display = "none";
+        }
+        if (overlay) {
+            overlay.style.display = "none";
+        }
+        return;
+    }
+
     // Target the video inside the clicked project item
     const video = event.currentTarget.querySelector('video');
 
@@ -302,8 +317,6 @@ window.scrollToAbout = function() {
         ease: "power4.inOut"
     });
 };
-
-
 
 
 
